c2b084418b92d56bd1eadc2b9f2a3e137c9d8c9e
Martin Hlaváč <hlavacm@hotmail.com>
2016-07-27 10:44:29 +0200

    2. Coding Dojo ČB
    Úvod do Coding Dojo, String Calculator Kata v C#

---
+++ 002-csharp-string-calculator/CodingDojo/Calculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CodingDojo
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            string delimiter = "";
+            if (numbers.StartsWith("//"))
+            {
+                delimiter = FindDelimiter(ref numbers);
+            }
+
+            if (numbers.EndsWith(",\n"))
+            {
+                throw new NotSupportedException();
+            }
+ 
+            // int sum = fragments.Sum(int.Parse);
+
+            int sum = 0;
+            foreach (string fragment in ExtractNumbers(numbers, delimiter))
+            {
+                int number;
+                if (int.TryParse(fragment, out number))
+                {
+                    sum += number;
+                }
+            }
+
+            return sum;
+        }
+
+        private static string FindDelimiter(ref string numbers)
+        {
+            string delimiter = "";
+            for (int i = 2, n = numbers.Length; i < n; i++)
+            {
+                char ch = numbers[i];
+                if (ch == '\n')
+                {
+                    numbers = numbers.Substring(0, i + 1);
+                    break;
+                }
+                delimiter += ch;
+            }
+            return delimiter;
+        }
+
+
+        public string[] ExtractNumbers(string numbers, string delimiter)
+        {
+            if (string.IsNullOrEmpty(delimiter))
+            {
+                delimiter = ",";
+            }
+
+            string[] fragments = Regex.Split(numbers, delimiter + "|\n");
+
+            return fragments;
+        }
+    }
+}
---
+++ 002-csharp-string-calculator/CodingDojo/CalculatorTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.Remoting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingDojo
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        public Calculator Calculator { get; private set; }
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Calculator = new Calculator();
+        }
+        
+        [TestMethod]
+        public void AddZeroTest()
+        {
+            Assert.AreEqual(0, Calculator.Add(""));
+        }
+
+        [TestMethod]
+        public void AddOneNumberTest()
+        {
+            Assert.AreEqual(1, Calculator.Add("1"));
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTest()
+        {
+            Assert.AreEqual(3, Calculator.Add("1,2"));
+        }
+
+        [TestMethod]
+        public void AddMoreNumbersTest()
+        {
+            Assert.AreEqual(10, Calculator.Add("1,2,1,1,1,1,3"));
+        }
+
+
+        [TestMethod]
+        public void AddNumbersWithNewLineDelimiterTest()
+        {
+            Assert.AreEqual(6, Calculator.Add("1\n2,3"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void InputStringShouldNotEndedWithNewLine()
+        {
+            //Assert.AreEqual(1, Calculator.Add("1,\n"));
+            Calculator.Add("1,\n");
+        }
+
+        [TestMethod]
+        public void DelimterTest()
+        {
+            Assert.AreEqual(6, Calculator.Add("//;\n1;2;3"));
+        }
+    }
+}
